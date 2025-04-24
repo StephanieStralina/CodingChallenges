67f982640603dce361e534fd16c87a425cbd0ecf
StephanieStralina <StephanieDRichter@gmail.com>
2025-04-24 15:59:35 -0500

    Python Challenges 2 and 3

--- challenges.py
+++ challenges.py
@@ -8,6 +8,20 @@
 # Array w/ peers test scores (not yours, add them in to calculate class average)
 # Compare average with your score, return true if you're better otherwise false
 def better_than_average(class_points, your_points):
-    # Your code here
     average = (sum(class_points) + your_points)/(len(class_points)+1)
     return your_points > average
+
+# Challenge 2
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+# For example, for [1, 2, 2] should return 9
+
+# Answer 2
+def square_sum(numbers):
+    return sum(x * x for x in numbers) 
+
+# Challenge 3
+# Implement a function which convert the given boolean value into its string representation.
+
+# Answer 3
+def boolean_to_string(b):
+    return str(b)
